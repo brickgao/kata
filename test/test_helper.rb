5ef02946ebef885992cea6e08cd3f696a460becf
brickgao <brickgao@gmail.com>
2016-06-05 16:35:53 +0800

    add travis-ci and coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
+require 'coveralls'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
+Coveralls.wear!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
