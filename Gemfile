43dea92682529a9e230e5193593d7c21b904e55f
brickgao <brickgao@gmail.com>
2016-06-10 15:24:59 +0800

    add test case to the feature that could create new users

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'bcrypt', '~> 3.1.7'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  # For mocking and stubbing
+  gem 'mocha'
 end
 
 group :development do
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,44 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should create successfully with valid params" do
+    new_user = mock()
+    new_user.stubs(:save).returns(true)
+    User.stubs(:new).returns(new_user)
+
+    post :create, post_params
+    assert_response :success
+    assert_equal @response.body, 'success'
+
+    User.unstub(:new)
+  end
+
+  test "should create unsuccessfully with invalid params" do
+    new_user, errors = mock(), mock()
+    new_user.stubs(:save).returns(false)
+    new_user.stubs(:errors).returns(errors)
+    [:name, :email].each do |attr|
+      new_user.stubs(attr).returns('placeholder')
+    end
+    errors.stubs(:any?).returns(false)
+    User.stubs(:new).returns(new_user)
+
+    post :create, post_params
+    assert_response :success
+    assert_not_equal @response.body, 'success'
+
+    User.unstub(:new)
+  end
+
+  private
+  
+    def post_params
+      {
+        user: {
+          name: 'testname', email: 'testmail',
+          password: 'badpasswd',
+          password_confirmation: 'badpasswd'
+        }
+      }
+    end
 end
