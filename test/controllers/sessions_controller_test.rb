a99312428f24c3474d049c76093dd9d8f3021f1b
brickgao <brickgao@gmail.com>
2016-06-11 16:22:20 +0800

    add test case for the feature that users could login

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,24 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
+
+  def setup
+    @user = users(:alice)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
 
+  test "login with valid information" do
+    post :create, session: { email: @user.email, password: 'badpassword' }
+    assert_redirected_to '/'
+  end
+
+  test "login with invalid information" do
+    post :create, session: { email: @user.email, password: 'invalid' }
+    assert_template 'new'
+    assert_select 'div.error-message'
+  end
 end
