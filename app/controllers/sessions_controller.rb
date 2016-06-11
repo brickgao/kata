64ee4d0c974cead82a993d64eeb6ad4d13378326
brickgao <brickgao@gmail.com>
2016-06-11 16:14:52 +0800

    add the feature that user could login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+
   def new
+    redirect_to '/' if logged_in?
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to '/'
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
-  
+
   def destory
   end
 end
