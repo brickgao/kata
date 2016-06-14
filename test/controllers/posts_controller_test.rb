5e7e0e05efbd852557ba20462614d531f726f577
brickgao <brickgao@gmail.com>
2016-06-14 12:11:57 +0800

    add test case for the feature that users could create posts

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,4 +4,41 @@ class PostsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create successfully with valid params" do
+    post_params = {
+      :post => {
+        :title => "Sample Title",
+        :body => "Sample Body",
+        :node => nodes(:testnode)
+      }
+    }
+    new_post = mock()
+    new_post.stubs(:save).returns(true)
+    Post.stubs(:new).returns(new_post)
+
+    post :create, post_params
+    assert_response :success
+    assert_equal @response.body, 'success'
+
+    Post.unstub(:new)
+  end
+
+  test "should create unsuccessfully with invalid params" do
+    post_params = {
+      :post => {
+        :title => "Sample",
+        :body => "Sample",
+        :node => nodes(:testnode)
+      }
+    }
+    post :create, post_params
+    assert_response :success
+    assert_template 'posts/new'
+    assert_select 'div.error-message'
+  end
 end
