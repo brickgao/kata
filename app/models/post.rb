e6b14d76b62d9e1db2798d48b0053e2998425c2a
brickgao <brickgao@gmail.com>
2016-06-25 18:21:44 +0800

    add comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :node
   belongs_to :user
+  has_many :comments
   validates :title, presence: true, length: { minimum: 8 }
   validates :body, presence: true, length: { minimum: 8 }
   validates :node, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts
+  has_many :comments
   before_save { self.email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
