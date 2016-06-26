bc6c212900187358c8c1aa051f72694b2d65bfb5
brickgao <brickgao@gmail.com>
2016-06-26 21:01:03 +0800

    add test case for comment controller

---
+++ test/controllers/comments_controller_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class CommentsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+    @post = posts(:testpost)
+    @user = users(:alice)
+    session[:user_id] = @user.id
+  end
+
+  test "should create successfully with valid params" do
+    post_params = {
+      :comment => {
+        :body => "test comment",
+        :post => @post.id
+      }
+    }
+    post :create, post_params
+    assert_response :redirect
+  end
+
+  test "should create unsuccessfully with empty body" do
+    post_params = {
+      :comment => {
+        :body => "",
+        :post => @post.id
+      }
+    }
+    post :create, post_params
+    assert_response :redirect
+    assert flash[:danger]
+  end
+end
