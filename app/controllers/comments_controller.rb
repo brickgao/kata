e3e62b5c1ea5a40b429684043f9c060c694b1fe9
brickgao <brickgao@gmail.com>
2016-07-09 13:58:14 +0800

    Support showing all comments created by all users or specific user in single page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,11 +10,23 @@ class CommentsController < ApplicationController
     redirect_to @comment.post
   end
 
+  def index
+    @query_page = (params[:page] || '1').to_i
+    conditions = {}
+    conditions[:user] = User.find(params[:user_id]) if params[:user_id]
+    @pages_total = (Comment.where(conditions).count / comments_limit.to_f).ceil
+    @comments = Comment.where(conditions).limit(comments_limit).offset((@query_page - 1) * comments_limit)
+  end
+
   private
     def post_params
       _params = params.require(:comment).permit(:body, :post)
       _params[:user] = User.find(session[:user_id])
       _params[:post] = Post.find(Integer(_params[:post]))
       _params
+    end
+
+    def comments_limit
+      10
     end
 end
