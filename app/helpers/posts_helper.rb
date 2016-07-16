05611bac653ffbbd880d064daa8f14320e922d8f
brickgao <brickgao@gmail.com>
2016-07-16 23:46:36 +0800

    support links in the posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,4 +3,14 @@ module PostsHelper
   def get_user
     @_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def parse_content(content)
+    last, ranges = 0, []
+    content.to_enum(:scan, /https?:\/\/[\S]+/i).map do |match|
+        ranges << [false, last, $`.size]
+        last = $`.size + match.length
+        ranges << [true, $`.size, last]
+    end
+    ranges << [false, last, content.length]
+  end
 end
