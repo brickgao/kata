693a8f35ddb68573171f94bf17f90bc429beadf2
brickgao <brickgao@gmail.com>
2016-08-08 23:25:05 +0800

    add test case

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -24,6 +24,25 @@ class MessagesControllerTest < ActionController::TestCase
     assert_response :redirect
   end
 
+  test "should get chat successful" do
+    get :show_chat, {:chat_with_id => @to.id }
+    assert_response :success
+    assert_template 'messages/show_chat'
+    assert_select '.chat'
+  end
+
+  test "should get chat unsuccessfully with invalid chat_with_id" do
+    get :show_chat, {:chat_with_id => 233333 }
+    assert_response :redirect
+  end
+
+  test "should get the count of unread messages" do
+    get :get_unread_messages_count
+    assert_response :success
+    result = JSON.parse(@response.body)
+    assert_equal result['unread_messages_count'], 1
+  end
+
   private
     def message_params
       {
