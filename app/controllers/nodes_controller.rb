d2b4626115ebad770d4e63a73a27db488d1e509d
brickgao <brickgao@gmail.com>
2016-08-15 23:30:14 +0800

    support creating node

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -4,4 +4,22 @@ class NodesController < ApplicationController
     nodes = Node.all
     render json: nodes.map { |node| { :id => node.id, :name => node.name } }
   end
+
+  def new
+    @node = Node.new 
+  end
+
+  def create
+    @node = Node.new post_params
+    if @node.save
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def post_params
+      _params = params.require(:node).permit(:title, :body)
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     flash[:warning] = 'You need to login'
     redirect_to '/login' if session[:user_id] == nil
   end
+
+  def authenticate_admin!
+    redirect_to '/' unless session[:user_id] != nil && User.find(session[:user_id]).user_type == 1
+  end
 end
