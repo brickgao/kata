a78bd7b386c6590ffc934825a11c655a959a78ae
brickgao <brickgao@gmail.com>
2016-09-01 18:37:32 +0800

    add test case

--- test/controllers/nodes_controller_test.rb
+++ test/controllers/nodes_controller_test.rb
@@ -7,8 +7,8 @@ class NodesControllerTest < ActionController::TestCase
   # end
 
   def setup
-    user = users(:alice)
-    session[:user_id] = user.id
+    @node, @user = nodes(:testnode), users(:alice)
+    session[:user_id] = @user.id
   end
 
   test "should return nodes" do
@@ -32,6 +32,48 @@ class NodesControllerTest < ActionController::TestCase
     _post_params = post_params
     _post_params[:node][:name] = ''
     post :create, _post_params
+    assert_response :success
+    assert_select 'div.error-message'
+  end
+
+  test "should create node unsuccessfully with empty summary" do
+    _post_params = post_params
+    _post_params[:node][:summary] = ''
+    post :create, _post_params
+    assert_response :success
+    assert_select 'div.error-message'
+  end
+
+  test "should create node unsuccessfully with short summary" do
+    _post_params = post_params
+    _post_params[:node][:summary] = 'a' * 4
+    post :create, _post_params
+    assert_response :success
+    assert_select 'div.error-message'
+  end
+
+  test "should create node unsuccessfully with long summary" do
+    _post_params = post_params
+    _post_params[:node][:summary] = 'a' * 51
+    post :create, _post_params
+    assert_response :success
+    assert_select 'div.error-message'
+  end
+
+  test "edit should be valid" do
+    get :edit, :id => @node.id
+    assert_response :success
+  end
+
+  test "should edit node successfully" do
+    patch :update, post_params.update(:id => @node.id)
+    assert_response :redirect
+  end
+
+  test "should edit node unsuccessfully with short summary" do
+    _post_params = post_params
+    _post_params[:node][:summary] = 'a' * 2
+    patch :create, _post_params.update(:id => @node.id)
     assert_response :success
     assert_select 'div.error-message'
   end
