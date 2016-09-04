7a37f3966016d8c9e43fadb4f6889ba3db22c7e6
brickgao <brickgao@gmail.com>
2016-09-04 16:53:58 +0800

    add highlight for the results of searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,24 @@
 class SearchController < ApplicationController
+  add_template_helper SearchHelper
+
   def search
     @words_search = params[:query]
     return redirect_to '/' if @words_search.nil?
     @posts = Post.search(
-        :query => { :match => { :title => @words_search } },
-        :highlight => { :fields => { :title => {} } }
+        :query => {
+            :multi_match => {
+                :query => @words_search,
+                :fields => ["title", "body"]
+            }
+        },
+        :highlight => { 
+            :fields => {
+                :title => {},
+                :body => {}
+            }
+        }
     )
+    @search_results = @posts.results
+    @search_records = @posts.records.records
   end
 end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,10 @@
 module SearchHelper
+  def generate_highlight_group(text)
+    result, flag = [], false
+    text.split(/<\/?em>/).each do |word|
+      result << [word, flag]
+      flag = !flag
+    end
+    result
+  end
 end
