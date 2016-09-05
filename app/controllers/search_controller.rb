a3000ab92a4d46f6d5dae9d9cfbbf25d334e8057
brickgao <brickgao@gmail.com>
2016-09-05 15:38:53 +0800

    support pagination in searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,14 @@
 class SearchController < ApplicationController
   add_template_helper SearchHelper
+  before_filter :authenticate_user!
 
   def search
     @words_search = params[:query]
     return redirect_to '/' if @words_search.nil?
+    @current_page = (params[:page] || '1').to_i
     @posts = Post.search(
+        :from => (@current_page - 1) * posts_limit,
+        :size => posts_limit,
         :query => {
             :multi_match => {
                 :query => @words_search,
@@ -18,7 +22,13 @@ class SearchController < ApplicationController
             }
         }
     )
+    @pages_total = (@posts.results.total / posts_limit.to_f).ceil
     @search_results = @posts.results
     @search_records = @posts.records.records
+  end
+
+private
+  def posts_limit
+    10
   end
 end
