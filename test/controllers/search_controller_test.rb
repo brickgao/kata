7e5f0a14ffbba2bad0c25d87654b4a2b2dd5e2bb
brickgao <brickgao@gmail.com>
2016-09-12 19:00:08 +0800

    add test case

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -4,4 +4,13 @@ class SearchControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:alice)
+    session[:user_id] = @user.id
+  end
+
+  test "should render successfully" do
+    get :search, query: "test"
+    assert_response :success
+  end
 end
